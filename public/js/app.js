beaef5d7c8a62066504f478b31bb8f6994e4890e
AlexG-BGLL-PC <2019119572@ub.edu.bz>
2023-11-17 15:08:36 -0600

    Initial Test

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+// app.js
+const socket = io('http://localhost:3000');
+
+function updateNumbers() {
+    const numbers = [
+        document.getElementById('numberInput1').value,
+        document.getElementById('numberInput2').value,
+        document.getElementById('numberInput3').value,
+        document.getElementById('numberInput4').value,
+        document.getElementById('numberInput5').value,
+        document.getElementById('numberInput6').value,
+        document.getElementById('numberInput7').value,
+        document.getElementById('numberInput8').value
+    ];
+
+    // Send the array of numbers to the server
+    socket.emit('updateNumbers', numbers);
+}
+
+// Listen for updated numbers from the server
+socket.on('numbersUpdated', (data) => {
+    displayNumbers(data);
+});
+
+function displayNumbers(numbers) {
+    const numbersList = document.getElementById('numbersList');
+
+    // Clear previous content
+    numbersList.innerHTML = '';
+
+    // Display each number in a list item
+    numbers.forEach((number, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Number ${index + 1}: ${number}`;
+        numbersList.appendChild(listItem);
+    });
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for numbers update from clients
+    socket.on('updateNumbers', (data) => {
+        // Broadcast the updated numbers to all connected clients
+        io.emit('numbersUpdated', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
