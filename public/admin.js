9fcbedb42f89836600c0222ceaccf4dfd0544217
AlexG-BGLL-PC <2019119572@ub.edu.bz>
2023-11-22 14:01:16 -0600

    updated files

---
+++ public/admin.js
@@ -0,0 +1,18 @@
+// admin.js
+const socket = io('https://bgll-display.netlify.app/admin'); // Replace with your Netlify site URL
+
+function updateNumbers() {
+    const numbers = [
+        document.getElementById('numberInput1').value,
+        document.getElementById('numberInput2').value,
+        document.getElementById('numberInput3').value,
+        document.getElementById('numberInput4').value,
+        document.getElementById('numberInput5').value,
+        document.getElementById('numberInput6').value,
+        document.getElementById('numberInput7').value,
+        document.getElementById('numberInput8').value
+    ];
+
+    // Send the array of numbers to the server
+    socket.emit('updateNumbers', numbers);
+}
--- public/js/display.js
+++ public/js/display.js
@@ -1,22 +1,6 @@
 // app.js
 const socket = io('https://bgll-display.netlify.app');
 
-function updateNumbers() {
-    const numbers = [
-        document.getElementById('numberInput1').value,
-        document.getElementById('numberInput2').value,
-        document.getElementById('numberInput3').value,
-        document.getElementById('numberInput4').value,
-        document.getElementById('numberInput5').value,
-        document.getElementById('numberInput6').value,
-        document.getElementById('numberInput7').value,
-        document.getElementById('numberInput8').value
-    ];
-
-    // Send the array of numbers to the server
-    socket.emit('updateNumbers', numbers);
-}
-
 // Listen for updated numbers from the server
 socket.on('numbersUpdated', (data) => {
     displayNumbers(data);
